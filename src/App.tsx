e419215ba1464ca54b970211bdaf846429b33d1d
Will <willerson.miranda@gmail.com>
2021-04-19 23:16:23 -0300

    pagina inicio estilizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 
-interface TitleProps {
-  text: string;
-}
+import { FiArrowRight } from 'react-icons/fi';
 
-function Title(props : TitleProps) {
-  return (
-    <h1>{props.text}</h1>
-  )
-}
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <Title text="Título 1" />
-      <Title text="Título 2" />
-      <Title text="Título 3" />
-      <Title text="Título 4" />
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>
+            São Paulo
+          </strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="" className="enter-app" >
+          <FiArrowRight size={26} color="rgb(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
-}
+}// 1:18:38
 
 export default App;
