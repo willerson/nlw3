a9036c3c6365cb8d8a228235152f2f4338e5bc8f
Will <willerson.miranda@gmail.com>
2021-04-30 20:41:01 -0300

    salvando imagens

--- src/controllers/OrphanagesControllers.ts
+++ src/controllers/OrphanagesControllers.ts
@@ -3,7 +3,24 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
+    async index(request: Request, response: Response) {
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.find({});
+
+        return response.json(orphanages);
+    },
+    async show(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+        return response.json(orphanage);
+    },
     async create(request: Request, response: Response) {
+
         const {
             name,
             latitude,
@@ -15,6 +32,11 @@ export default {
         } = request.body;
     
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
     
         const orphanage = orphanagesRepository.create({
             name,
@@ -24,6 +46,7 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends,
+            images
         });
     
         await orphanagesRepository.save(orphanage);
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesControllers';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 // MVC
 // Model
 // Views
-// Controllers 01:04:09
+// Controllers
 
-
-routes.post('/orphanages', OrphanagesController.create);
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 export default routes;
