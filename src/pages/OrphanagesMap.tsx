f36e770813dd167f70ad2d5feb3d899eab27d6db
Will <willerson.miranda@gmail.com>
2021-05-03 22:23:57 -0300

    páginas criadas

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2],
+})
 
 function OrphanagesMap() {
     return (
@@ -16,7 +26,7 @@ function OrphanagesMap() {
                 <header>
                     <img src={mapMarkerImg} alt="Happy"/>
 
-                    <h2>Escolha um organato no mapa</h2>
+                    <h2>Escolha um orfanato no mapa</h2>
                     <p>Muitas crianças estão esperando a sua visita :)</p>
                 </header>
 
@@ -26,14 +36,25 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <MapContainer 
+            <MapContainer
                 center={[-23.6941423,-46.778253]}
                 zoom={15}
                 style={{ width: '100%', height: '100%' }}
             >
             <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker 
+                icon={mapIcon}
+                position={[-23.6941423,-46.778253]}
+            >
+                <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                    Lar das meninas
+                    <Link to="/orphanages/1">
+                        <FiArrowRight size={20} color="#FFF" />
+                    </Link>
+                </Popup>
+            </Marker>
             </MapContainer>
-            <Link to="" className='create-orphanage'>
+            <Link to="/orphanages/create" className='create-orphanage'>
                 <FiPlus size={32} color="#fff" />
             </Link>
         </div>
