944c17f301b44b6167f516f87417b9a50b50ad75
Will <willerson.miranda@gmail.com>
2021-05-18 23:27:20 -0300

    Aula 4 finalizada

--- App.tsx
+++ App.tsx
@@ -1,45 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View,  Dimensions } from 'react-native';
-import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 
-import mapMarker from './src/images/map-marker.png';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+
+import Routes from './src/routes';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <MapView 
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -23.7277745,
-          longitude: -46.778948,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker 
-          icon={mapMarker}
-          coordinate={{
-            latitude: -23.7277745,
-            longitude: -46.778948,
-          }}
-        >
-          <Callout>
-            <Text>Lar das meninas</Text>
-          </Callout>
-        </Marker>
-      </MapView>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  }
-});
